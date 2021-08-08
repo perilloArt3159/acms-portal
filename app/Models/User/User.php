c4783ad37ba5863b177a492d43d455c227a6b755
zen <artperillo.ictdu@spcf.edu.ph>
2021-08-08 21:20:44 +0800

    :hammer: Modified model.stub

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, Sluggable;
 
+    protected $table = "users";
+
     /**
      * The attributes that are mass assignable.
      *
