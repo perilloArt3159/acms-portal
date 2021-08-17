88f62a28e1f2cebc87a286ebc185af182dcd092b
zen <artperillo.ictdu@spcf.edu.ph>
2021-08-17 12:21:26 +0800

    :hammer: Modified model.stub

--- database/migrations/2021_08_12_145848_add_member_id_foreign_to_users_table.php
+++ database/migrations/2021_08_12_145848_add_member_id_foreign_to_users_table.php
@@ -14,7 +14,7 @@ class AddMemberIdForeignToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('member_id')->constrained('members')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('member_id')->after('id')->constrained('members')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
